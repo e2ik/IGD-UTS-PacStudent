1c9e4da9b5f9ef53e9d6d28767db6f528ab5d87e
e2ik <erik.jensen.lin@gmail.com>
2023-09-27 01:50:01 +1000

    prelim level generator quadrant complete

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -45,12 +45,58 @@ public class LevelGenerator : MonoBehaviour
 
     void GenerateLevel()
     {
+        Vector3 startPos = new Vector3(-15f,10f,0.0f);
+        GameObject level_1 = new GameObject("Level01_Generated");
+        level_1.transform.position = startPos;
+        GameObject topLeftQuad = new GameObject("TopLeftQuad");
+        topLeftQuad.transform.parent = level_1.transform;
+        topLeftQuad.transform.localPosition = Vector3.zero;
+
         for (int row = 0; row < numRows; row++) {
             for (int col = 0; col < numCols; col++) {
                 int cell = levelMap[row, col];
+                int cell_right = (col < numCols - 1) ? levelMap[row, col + 1] : -1;
+                int cell_left = (col > 0) ? levelMap[row, col - 1] : -1;
+                int cell_down = (row < numRows - 1) ? levelMap[row + 1, col] : -1;
+                int cell_up = (row > 0) ? levelMap[row - 1, col] : -1;
+                GameObject tile = GetSegment(cell);
 
+                if (tile != null) { // adding level_1 position to set it at global
+                    Vector3 pos = level_1.transform.position + new Vector3(col, -row, 0);
+                    GameObject segment;
 
-
+                    if (cell == 2 && (cell_down == 2 || cell_up == 2)) {
+                        segment = Instantiate(tile, pos, Quaternion.Euler(0,0,90));
+                    } else if (cell == 2 && (cell_right == 2 || cell_left == 2)) {
+                        segment = Instantiate(tile, pos, Quaternion.Euler(0,0,0));
+                    } else if (cell == 3 && (cell_right == 5 || cell_right == 0) && (cell_up == 5 || cell_up == 0)) {
+                        segment = Instantiate(tile, pos, Quaternion.Euler(0,0,270));
+                    } else if (cell == 3 && (cell_left == 5 || cell_left == 0) && (cell_down == 5 || cell_down == 0)) {
+                        segment = Instantiate(tile, pos, Quaternion.Euler(0,0,90));
+                    } else if (cell == 3 && (cell_right == 5 || cell_right == 0) && (cell_down == 5 || cell_down == 0)) {
+                        segment = Instantiate(tile, pos, Quaternion.Euler(0,0,180));
+                    } else if (cell == 3 && cell_right == 4 && cell_down == 4) {
+                        segment = Instantiate(tile, pos, Quaternion.Euler(0,0,0));
+                    } else if (cell == 3 && cell_right == 4 && cell_up == 4) {
+                        segment = Instantiate(tile, pos, Quaternion.Euler(0,0,90));
+                    } else if (cell == 3 && cell_left == 4 && cell_down == 4) {
+                        segment = Instantiate(tile, pos, Quaternion.Euler(0,0,270));
+                    } else if (cell == 1 && cell_up == 2 && cell_left == 2) {
+                        segment = Instantiate(tile, pos, Quaternion.Euler(0,0,180));
+                    } else if (cell == 1 && cell_down == 2 && cell_left == 2) {
+                        segment = Instantiate(tile, pos, Quaternion.Euler(0,0,270));
+                    } else if (cell == 4 && (cell_up == 5 || cell_down == 5 || cell_up == 0 || cell_down == 0)) {
+                        segment = Instantiate(tile, pos, Quaternion.Euler(0,0,90));
+                    } else if (cell == 4 && (cell_left == 5 || cell_right == 5 || cell_left == 0 || cell_right == 0)) {
+                        segment = Instantiate(tile, pos, Quaternion.Euler(0,0,0));
+                    } else if (cell == 1 && cell_up == 2 && cell_right == 2) {
+                        segment = Instantiate(tile, pos, Quaternion.Euler(0,0,90));
+                    } else {
+                        segment = Instantiate(tile, pos, Quaternion.identity);
+                    }
+                    segment.transform.parent = topLeftQuad.transform;
+                }
+            
             }
         }
     }
