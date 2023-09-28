09f5e8a2386f79361e8f8fd14dce0b7d96dbfdeb
e2ik <erik.jensen.lin@gmail.com>
2023-09-28 17:05:20 +1000

    removed bounds calc, just using numRows  for orthSize

--- Assets/Scripts/CenterCamera.cs
+++ Assets/Scripts/CenterCamera.cs
@@ -22,23 +22,12 @@ public class CenterCamera : MonoBehaviour
                 numCols = levelGenerator.numCols;
             }
         }
-        Debug.Log(numRows + " " + numCols);
         Vector3 lvlPos = level.transform.position;
         float newX = (numCols + (numCols-1)) /2;
         float newY = (numRows + (numRows-2)) /2;
         setCamPos = new Vector3(lvlPos.x + newX, lvlPos.y - newY, lvlPos.z - 4);
         cam.transform.position = setCamPos;
-        FitCameraToObjects();
+        cam.orthographicSize = (float)(numRows + 1) + padding/10;
 
-    }
-    private void FitCameraToObjects() {
-        Bounds bounds = new Bounds(Vector3.zero, Vector3.zero);
-        GameObject[] allObjects = GameObject.FindObjectsOfType<GameObject>();
-
-        foreach (GameObject obj in allObjects) {
-            if (obj.name.ToLower().Contains("wall")) { bounds.Encapsulate(obj.transform.position); }
-        }
-        float newSize = Mathf.Max(bounds.size.x, bounds.size.y) * 0.5f + 2.0f + padding;
-        cam.orthographicSize = newSize;
     }
 }
