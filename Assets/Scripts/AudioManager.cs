9bbd62ba99d79810079e5bbe5efc3c278b56f426
e2ik <erik.jensen.lin@gmail.com>
2023-09-29 00:02:40 +1000

    original changes

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,18 +1,31 @@
 using System.Collections;
+using UnityEngine.Events;
 using System.Collections.Generic;
+
 using UnityEngine;
 
 public class StartMusicThenGhost : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private AudioClip intro;
+    [SerializeField] private AudioClip ghostMusic;
+    public UnityEvent onGhostStart;
+    private AudioSource clips;
     void Start()
     {
-        
+        clips = GetComponent<AudioSource>();
+        StartCoroutine(delayAfterStart());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private IEnumerator delayAfterStart() {
+
+        clips.clip = intro;
+        clips.Play();
+
+        yield return new WaitForSeconds(1.5f);
+
+        clips.clip = ghostMusic;
+        clips.loop = true;
+        clips.Play();
+        onGhostStart.Invoke();
     }
 }
